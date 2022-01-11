458ce86eb7d08a9f3b9340d5d66025c4a5ad362e
Slluxx <Slluxx@users.noreply.github.com>
2022-01-11 23:54:43 +0100

    Update update.py

--- update.py
+++ update.py
@@ -3,10 +3,15 @@ import os, json, requests
 if not os.path.exists('apiData'):
     os.makedirs('apiData')
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+    'cache-control': 'private, max-age=0, no-cache, no-store, must-revalidate'
+}
+
 with open('mappings.json') as json_file:
     mappings = json.load(json_file)
     for lang in mappings:
-        r = requests.get(mappings[lang]['apiUrl'], headers={'Cache-Control': 'no-cache'})
+        r = requests.get(mappings[lang]['apiUrl'], headers=headers)
         rContent = r.content
         try:
             a_json = json.loads(rContent)
