02ebe93173bd19217f3c130a79ec3b4d3d53a16e
Slluxx <Slluxx@users.noreply.github.com>
2022-01-11 22:24:06 +0100

    Update update.py

--- update.py
+++ update.py
@@ -10,11 +10,6 @@ with open('mappings.json') as json_file:
         rContent = r.content
         try:
             a_json = json.loads(rContent)
-
-            # if file exists, delete it
-            if os.path.exists('apiData/' + lang + '.json'):
-                os.remove('apiData/' + lang + '.json')
-
             with open('apiData/' + lang + '.json', 'wb') as f:
                 f.write(rContent)
         except:
