fb053d93ae4f6764f7fbe00ac8390e1dffdd12d0
Slluxx <Slluxx@users.noreply.github.com>
2022-01-13 01:34:28 +0100

    updated mapping toinclude proxys
    In case anyone wonders, even though the pokemon tv api has region based endpoints (en, de, ...), their data is still dynamic based on the region you are requesting data from. Proxys are needed to get all the content available in a region without messing anything up (which it did before) because the data was always requested from germany (locally) or from github actions - wherever that is located.

--- update.py
+++ update.py
@@ -1,26 +1,53 @@
-import os, json, requests, time
+import json, requests, os
+
+mappings = {}
+
+def getProxies(url):
+    try:
+        r = requests.get(url)
+        a_json = json.loads(r.content)
+        return a_json
+    except:
+        return []
+
+def getAPIData(lang, mapping):
+    proxiesArr = getProxies(mapping["proxyApiUrl"])
+    url = mapping['apiUrl']
+
+    if len(proxiesArr) > 0:
+        for proxy in proxiesArr:
+            addr = f"{proxy['Type'][0].lower()}://{proxy['Ip']}:{proxy['Port']}"
+            try:
+                r = requests.get(url, proxies={"http": addr})
+                a_json = json.loads(r.content)
+                mappings[lang]['lastProxyFetchSuccess'] = True
+                return a_json
+            except:
+                continue
+        print("No proxies available for this country: " + lang)
+        print("Trying without proxy...")
+
+    try:
+        r = requests.get(url)
+        a_json = json.loads(r.content)
+        mappings[lang]['lastProxyFetchSuccess'] = False
+        return a_json
+    except:
+        print("Could not fetch data for this country: " + lang)
+        return []
+
 
 if not os.path.exists('./apiData'):
     os.makedirs('./apiData')
 
-
-with open('mappings.json') as json_file:
+with open('./mappings.json') as json_file:
     mappings = json.load(json_file)
-    for lang in mappings:
-        currTime = str(int(time.time()))
-        try:
-            r = requests.get(mappings[lang]['apiUrl'])
-            rContent = r.content
-        except:
-            print("Error getting " + mappings[lang]['apiUrl'])
-            continue
-        try:
-            a_json = json.loads(rContent)
-            with open('./apiData/' + lang + '.json', 'wb') as f:
-                f.write(rContent)
 
-                if lang == "us":
-                    print(a_json[0])
-        except:
-            print(mappings[lang]['apiUrl'])
-            print("Could not be converted to JSON")
+for lang in mappings:
+    data = getAPIData(lang, mappings[lang])
+    if not data == []:
+        with open('./apiData/' + lang + '.json', 'w') as f:
+            f.write(json.dumps(data, indent=4))
+
+with open('./mappings.json', 'w') as f:
+    f.write(json.dumps(mappings, indent=4))
